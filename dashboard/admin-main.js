6d68e30c46db44b55cee48bf4f3b91bf2bc0789a
ho <howardxsheng@gmail.com>
2025-12-16 13:37:51 +0800

    feat: render orders list and delete single order

--- dashboard/admin-main.js
+++ dashboard/admin-main.js
@@ -1,3 +1,8 @@
+const BASE_URL = 'https://livejs-api.hexschool.io/api/livejs/v1';
+const API_PATH = 'ho';
+const ADMIN_TOKEN = '98X6tvEMbQSon1wN2Z2Y9Wqf8iF3';
+
+const orderTableDOM = document.querySelector('.orderPage-table')
 // C3.js
 let chart = c3.generate({
     bindto: '#chart', // HTML 元素綁定
@@ -17,3 +22,86 @@ let chart = c3.generate({
         }
     },
 });
+
+
+async function fetchOrders(){
+    const res = await axios.get(`${BASE_URL}/admin/${API_PATH}/orders`, {
+    headers: { Authorization: ADMIN_TOKEN }});
+    const orders = res.data.orders;
+    return orders
+};
+
+async function init() {
+    try {
+        orders = await fetchOrders();
+        console.log(orders)
+        renderOrders(orders)
+      
+
+    } catch (error) {
+        console.error("失敗：", error);
+    }
+};
+
+function renderOrders(dataToRender){
+    const ordersHTML = dataToRender.map((order) => {
+        return`
+        <tr>
+            <td>${order.id}</td>
+            <td>
+                <p>${order.user.name}</p>
+                <p>${order.user.tel}</p>
+            </td>
+            <td>${order.user.address}</td>
+            <td>${order.user.email}</td>
+            <td>
+                <p>${order.products[0].title}</p>
+            </td>
+            <td>${new Date(order.updatedAt * 1000).toLocaleDateString()}</td>
+            <td class="orderStatus">
+                <a href="#">未處理</a>
+            </td>
+            <td>
+                <input type="button" class="delSingleOrder-Btn" value="刪除" data-id="${order.id}">
+            </td>
+        </tr>
+        `
+    }).join("");
+    const headerHTML = `<thead>
+                    <tr>
+                        <th>訂單編號</th>
+                        <th>聯絡人</th>
+                        <th>聯絡地址</th>
+                        <th>電子郵件</th>
+                        <th>訂單品項</th>
+                        <th>訂單日期</th>
+                        <th>訂單狀態</th>
+                        <th>操作</th>
+                    </tr>
+                </thead>`
+    orderTableDOM.innerHTML = headerHTML + ordersHTML;
+}
+
+
+init();
+
+
+orderTableDOM.addEventListener('click', async (e) => {
+  const btn = e.target.closest('.delSingleOrder-Btn');
+  if (!btn) return;
+
+  const orderId = btn.dataset.id;
+  if (!orderId) return;
+
+  try {
+    const res = await axios.delete(
+      `${BASE_URL}/admin/${API_PATH}/orders/${orderId}`,
+      { headers: { Authorization: ADMIN_TOKEN } }
+    );
+    const orders = res.data.orders;  
+    renderOrders(orders);
+  } catch (err) {
+    console.error('刪除訂單失敗', err.response?.data || err);
+  }
+});
+
